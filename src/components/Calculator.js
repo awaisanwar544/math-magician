94c5c26e9c7d91b252bda5280d8b8ff52f991395
awaisanwar544 <awaisanwar544@gmail.com>
2022-03-15 03:47:21 +0500

    Add calculator ui

---
+++ src/components/Calculator.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import CalcKey from './calcComponents/CalcKey';
+import Display from './calcComponents/Display';
+
+import './Calculator.css';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { result: 0 };
+    this.buttons = [
+      {
+        text: 'AC',
+        type: 'other',
+      },
+      {
+        text: '+/-',
+        type: 'other',
+      },
+      {
+        text: '%',
+        type: 'other',
+      },
+      {
+        text: '÷',
+        type: 'operator',
+      },
+      {
+        text: '7',
+        type: 'other',
+      },
+      {
+        text: '8',
+        type: 'other',
+      },
+      {
+        text: '9',
+        type: 'other',
+      },
+      {
+        text: 'x',
+        type: 'operator',
+      },
+      {
+        text: '4',
+        type: 'other',
+      },
+      {
+        text: '5',
+        type: 'other',
+      },
+      {
+        text: '6',
+        type: 'other',
+      },
+      {
+        text: '-',
+        type: 'operator',
+      },
+      {
+        text: '1',
+        type: 'other',
+      },
+      {
+        text: '2',
+        type: 'other',
+      },
+      {
+        text: '3',
+        type: 'other',
+      },
+      {
+        text: '+',
+        type: 'operator',
+      },
+      {
+        text: '0',
+        type: 'other span-2-col',
+      },
+      {
+        text: '.',
+        type: 'other',
+      },
+      {
+        text: '=',
+        type: 'operator',
+      },
+    ];
+  }
+
+  render() {
+    const { result } = this.state;
+    return (
+      <div className="calc-container">
+        <div>
+          <Display value={result} />
+        </div>
+        <div className="keypad">
+          {this.buttons.map((item) => (
+            <CalcKey
+              key={item.text}
+              type={item.type}
+              keyValue={item.text}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
