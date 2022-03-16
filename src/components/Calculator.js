13de30cf430e35302ff2da53b60fe44fffebea25
awaisanwar544 <awaisanwar544@gmail.com>
2022-03-16 22:36:34 +0500

    Refactor class component to functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,131 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import CalcKey from './calcComponents/CalcKey';
 import Display from './calcComponents/Display';
 
 import './Calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.result = '0';
-    this.buttons = [
-      {
-        text: 'AC',
-        type: 'other',
-      },
-      {
-        text: '+/-',
-        type: 'other',
-      },
-      {
-        text: '%',
-        type: 'other',
-      },
-      {
-        text: '÷',
-        type: 'operator',
-      },
-      {
-        text: '7',
-        type: 'other',
-      },
-      {
-        text: '8',
-        type: 'other',
-      },
-      {
-        text: '9',
-        type: 'other',
-      },
-      {
-        text: 'x',
-        type: 'operator',
-      },
-      {
-        text: '4',
-        type: 'other',
-      },
-      {
-        text: '5',
-        type: 'other',
-      },
-      {
-        text: '6',
-        type: 'other',
-      },
-      {
-        text: '-',
-        type: 'operator',
-      },
-      {
-        text: '1',
-        type: 'other',
-      },
-      {
-        text: '2',
-        type: 'other',
-      },
-      {
-        text: '3',
-        type: 'other',
-      },
-      {
-        text: '+',
-        type: 'operator',
-      },
-      {
-        text: '0',
-        type: 'other span-2-col',
-      },
-      {
-        text: '.',
-        type: 'other',
-      },
-      {
-        text: '=',
-        type: 'operator',
-      },
-    ];
-  }
+const Calculator = () => {
+  let result = '0';
+  const buttons = [
+    {
+      text: 'AC',
+      type: 'other',
+    },
+    {
+      text: '+/-',
+      type: 'other',
+    },
+    {
+      text: '%',
+      type: 'other',
+    },
+    {
+      text: '÷',
+      type: 'operator',
+    },
+    {
+      text: '7',
+      type: 'other',
+    },
+    {
+      text: '8',
+      type: 'other',
+    },
+    {
+      text: '9',
+      type: 'other',
+    },
+    {
+      text: 'x',
+      type: 'operator',
+    },
+    {
+      text: '4',
+      type: 'other',
+    },
+    {
+      text: '5',
+      type: 'other',
+    },
+    {
+      text: '6',
+      type: 'other',
+    },
+    {
+      text: '-',
+      type: 'operator',
+    },
+    {
+      text: '1',
+      type: 'other',
+    },
+    {
+      text: '2',
+      type: 'other',
+    },
+    {
+      text: '3',
+      type: 'other',
+    },
+    {
+      text: '+',
+      type: 'operator',
+    },
+    {
+      text: '0',
+      type: 'other span-2-col',
+    },
+    {
+      text: '.',
+      type: 'other',
+    },
+    {
+      text: '=',
+      type: 'operator',
+    },
+  ];
 
-  handleClick(e) {
-    const newState = calculate(this.state, e.target.value);
-    this.setState({ ...newState }, () => {
-    });
-  }
+  const [calculations, setCalculations] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  updateResult = () => {
-    const { total, next } = this.state;
+  const handleClick = (e) => {
+    const newState = calculate(calculations, e.target.value);
+    setCalculations(newState);
+  };
+
+  const updateResult = () => {
+    const { total, next } = calculations;
     if (total === null && next === null) return '0';
     if (next === null) return total;
     return next;
   };
 
-  render() {
-    this.result = this.updateResult();
-    return (
-      <div className="calc-container">
-        <Display value={this.result} />
-        <div className="keypad">
-          {this.buttons.map((item) => (
-            <CalcKey
-              key={item.text}
-              type={item.type}
-              keyValue={item.text}
-              onClick={this.handleClick}
-            />
-          ))}
-        </div>
+  result = updateResult();
+
+  return (
+    <div className="calc-container">
+      <Display value={result} />
+      <div className="keypad">
+        {buttons.map((item) => (
+          <CalcKey
+            key={item.text}
+            type={item.type}
+            keyValue={item.text}
+            onClick={handleClick}
+          />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
